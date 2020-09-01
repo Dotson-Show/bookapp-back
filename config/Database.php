c92eb2ff5c8ef9234faefc0644eb23bc54915708
Dotun Showunmi <showunmioludotun@gmail.com>
2020-09-01 03:56:17 +0100

    connect remote

--- config/Database.php
+++ config/Database.php
@@ -1,11 +1,18 @@
 <?php
 
 class Database {
-    //DB Params
-    private $host = 'localhost';
-    private $dbName = 'books_db';
-    private $username = 'rootuser';
-    private $password = 'rejoyce4me';
+    // //DB Params local
+    // private $host = 'localhost';
+    // private $dbName = 'books_db';
+    // private $username = 'rootuser';
+    // private $password = 'rejoyce4me';
+    // private $conn;
+
+    //DB Params remote
+    private $host = 'q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    private $dbName = 'fw9xkvptjika3qyv';
+    private $username = 'rgtzd83yynt046c6';
+    private $password = 'pbuoxs99o3tl5y01';
     private $conn;
 
     //DB Connect
